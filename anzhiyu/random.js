ab67982156bd7f14687c545142e697b8fb7e2eeb
diyanpeng <13582630415@163.com>
2024-04-15 16:09:57 +0800

    Site updated: 2024-04-15 16:09:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/11/hello-world/","2024/04/15/hello-world - 副本/"];function toRandomPost(){
+var posts=["2024/04/15/hello-world - 副本/","2024/04/11/hello-world/","2024/04/15/护网蓝队、红队/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
